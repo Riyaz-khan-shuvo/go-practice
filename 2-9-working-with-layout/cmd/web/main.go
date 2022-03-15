31a43b47109596a150a3c6fe77fea969f2164da8
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2022-03-15 14:37:13 +0600

    2-9 complete

--- 2-9-working-with-layout/cmd/web/main.go
+++ 2-9-working-with-layout/cmd/web/main.go
@@ -11,7 +11,6 @@ const portNumber string = ":9000"
 
 func main() {
 	fmt.Println("listing the port http://localhost" + portNumber)
-
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 	http.ListenAndServe(portNumber, nil)
--- 2-9-working-with-layout/pkg/render/render.go
+++ 2-9-working-with-layout/pkg/render/render.go
@@ -3,8 +3,11 @@ package render
 import (
 	"fmt"
 	"net/http"
+	"path/filepath"
 	"text/template"
 )
+
+var functions = template.FuncMap{}
 
 func RenderTemplate(w http.ResponseWriter, tem string) {
 	pToTem, err := template.ParseFiles("../../template/" + tem)
@@ -13,5 +16,45 @@ func RenderTemplate(w http.ResponseWriter, tem string) {
 		fmt.Println(err)
 	}
 	pToTem.Execute(w, nil)
+
+	RenderTemplateTest(w)
+}
+
+func RenderTemplateTest(w http.ResponseWriter) (map[string]*template.Template, error) {
+
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("../../template/*.gohtml")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		// template set
+
+		fmt.Println("Currently page is : ", page)
+
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		matches, err := filepath.Glob("../../template/.*layouthtml")
+		if err != nil {
+			fmt.Println(err)
+			return myCache, err
+		}
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("../../template/.*layouthtml")
+			if err != nil {
+				fmt.Println(err)
+				return myCache, err
+			}
+		}
+		myCache[name] = ts
+	}
+	return myCache, nil
 
 }
